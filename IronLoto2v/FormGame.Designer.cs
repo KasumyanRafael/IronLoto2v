37795a134ba00984f35e6a039f6c1f370fc48087
KasumyanRafael <Kasumyan2004@mail.ru>
2022-04-10 12:50:26 +0300

    Убрали картинку

--- IronLoto2v/FormGame.Designer.cs
+++ IronLoto2v/FormGame.Designer.cs
@@ -28,13 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGame));
             this.dataGridViewGamer1 = new System.Windows.Forms.DataGridView();
             this.dataGridViewGamer2 = new System.Windows.Forms.DataGridView();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGamer1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGamer2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewGamer1
@@ -59,16 +56,6 @@
             this.dataGridViewGamer2.Size = new System.Drawing.Size(510, 558);
             this.dataGridViewGamer2.TabIndex = 3;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1445, 654);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
             // FormGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -76,13 +63,11 @@
             this.ClientSize = new System.Drawing.Size(1445, 654);
             this.Controls.Add(this.dataGridViewGamer2);
             this.Controls.Add(this.dataGridViewGamer1);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "FormGame";
             this.Text = "Осетинское лото";
             this.Load += new System.EventHandler(this.FormGame_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGamer1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGamer2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -90,6 +75,5 @@
         #endregion
         private System.Windows.Forms.DataGridView dataGridViewGamer1;
         private System.Windows.Forms.DataGridView dataGridViewGamer2;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
