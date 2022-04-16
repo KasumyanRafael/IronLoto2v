1b1e29a5c95774b880e583ede795c71a94917b4a
KasumyanRafael <Kasumyan2004@mail.ru>
2022-04-16 21:34:52 +0300

    some changes

--- IronLoto2v/FormGame.cs
+++ IronLoto2v/FormGame.cs
@@ -25,7 +25,6 @@ namespace IronLoto2v
         }
         void drawData(DataGridView data, int a, int b)
         {
-            int cnt = 0;
             DataGridViewImageColumn[] columns = new DataGridViewImageColumn[b];
             for (int i = 0; i < b; i++)
             {
@@ -36,14 +35,13 @@ namespace IronLoto2v
             data.Rows.Add(a - 1);
             data.RowTemplate.Height = 510 / a;
 
-
         }
         private void FormGame_Load(object sender, EventArgs e)
         {
             drawData(dataGridViewGamer1, y, x);
             drawData(dataGridViewGamer2, y, x);
             string[] s = Properties.Resources.dictionary.Split('\n');
-            //MessageBox.Show(s[6]);  
+            //MessageBox.Show(s[6]);
         }
 
         private void dataGridViewGamer1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -55,6 +53,7 @@ namespace IronLoto2v
         {
             MessageBox.Show("В данной игре запрещено ходить мышкой. Смысл в конкуренции)");
         }
+
     }
 }
 
