8a7722ee1d04306fee77e8de009bbc9061882554
KasumyanRafael <Kasumyan2004@mail.ru>
2022-04-17 18:07:16 +0300

    pictures

--- IronLoto2v/FormGame.cs
+++ IronLoto2v/FormGame.cs
@@ -30,7 +30,7 @@ namespace IronLoto2v
             {
                 columns[i] = new DataGridViewImageColumn();
                 columns[i].Width = 558 / b;
-                columns[i].Image = Image.FromFile(Directory.GetCurrentDirectory() + "/ImagesWithIronMeaning/6.png");
+                columns[i].Image = Image.FromFile(Directory.GetCurrentDirectory() + "/ImagesWithIronMeaning/8.png");
             }
             data.Columns.AddRange(columns);
             data.Rows.Add(a - 1);
