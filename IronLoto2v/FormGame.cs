c08cedb87ab164133b1ee18cb611e49a105e77b6
KasumyanRafael <Kasumyan2004@mail.ru>
2022-04-26 22:11:09 +0300

    Официальное добавление таймера, элементы клавиатурного управления

--- IronLoto2v/FormGame.cs
+++ IronLoto2v/FormGame.cs
@@ -21,6 +21,7 @@ namespace IronLoto2v
         int t = 2;
         int cnt = 0;
         string[] s;
+        word[] list;
 
         public FormGame()
         {
@@ -43,22 +44,16 @@ namespace IronLoto2v
         {
             drawData(dataGridViewGamer1, x, y);
             drawData(dataGridViewGamer2, x, y);
+            dataGridViewGamer1.CurrentCell = this.dataGridViewGamer1[0, 0];
+            dataGridViewGamer2.CurrentCell = this.dataGridViewGamer2[0, 0];
             s = Properties.Resources.dictionary.Split('\n');
+            list = ToWord(s);
+            antirepeat(list,s);
             filling(dataGridViewGamer1,s,x,y);
             filling(dataGridViewGamer2, s, x, y);
             timerChangePicture.Enabled = true;
             timerChangePicture.Interval = t*1000;
             
-        }
-
-        private void dataGridViewGamer1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            MessageBox.Show("В данной игре запрещено ходить мышкой. Смысл в конкуренции)");
-        }
-
-        private void dataGridViewGamer2_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            MessageBox.Show("В данной игре запрещено ходить мышкой. Смысл в конкуренции)");
         }
         void filling(DataGridView data,string[]array,int a,int b)
         {
@@ -73,18 +68,97 @@ namespace IronLoto2v
                 }
             }
         }
-        void antirepeat(int[]perm,int n)
-        { 
+        void antirepeat(word[]perm,string[]s)
+        {
             Random r = new Random(); // не создавайте новый Random здесь!
                                      // а то значения будут одни и те же
+            int n = s.Length;
             for (int i = n - 1; i >= 1; i--)
             {
                 int j = r.Next(i + 1);
                 // exchange perm[j] and perm[i]
-                int temp = perm[j];
+                word temp = perm[j];
                 perm[j] = perm[i];
                 perm[i] = temp;
             }
+        }
+
+        private void dataGridViewGamer1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("В данной игре запрещено ходить мышкой. Смысл в конкуренции)");
+        }
+
+        private void dataGridViewGamer2_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("В данной игре запрещено ходить мышкой. Смысл в конкуренции)");
+        }
+
+        private void FormGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D)
+            {
+                int col = dataGridViewGamer1.CurrentCell.ColumnIndex + 1;
+                int row = dataGridViewGamer1.CurrentCell.RowIndex;
+                dataGridViewGamer1.CurrentCell = dataGridViewGamer1[col, row];
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                int col = dataGridViewGamer1.CurrentCell.ColumnIndex;
+                int row = dataGridViewGamer1.CurrentCell.RowIndex - 1;
+                dataGridViewGamer1.CurrentCell = dataGridViewGamer1[col, row];
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                int col = dataGridViewGamer1.CurrentCell.ColumnIndex - 1;
+                int row = dataGridViewGamer1.CurrentCell.RowIndex;
+                dataGridViewGamer1.CurrentCell = dataGridViewGamer1[col, row];
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                int col = dataGridViewGamer1.CurrentCell.ColumnIndex;
+                int row = dataGridViewGamer1.CurrentCell.RowIndex + 1;
+                dataGridViewGamer1.CurrentCell = dataGridViewGamer1[col, row];
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                int col = dataGridViewGamer2.CurrentCell.ColumnIndex;
+                int row = dataGridViewGamer2.CurrentCell.RowIndex - 1;
+                dataGridViewGamer2.CurrentCell = dataGridViewGamer2[col, row];
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                int col = dataGridViewGamer2.CurrentCell.ColumnIndex;
+                int row = dataGridViewGamer2.CurrentCell.RowIndex + 1;
+                dataGridViewGamer2.CurrentCell = dataGridViewGamer2[col, row];
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                int col = dataGridViewGamer2.CurrentCell.ColumnIndex - 1;
+                int row = dataGridViewGamer2.CurrentCell.RowIndex;
+                dataGridViewGamer2.CurrentCell = dataGridViewGamer2[col, row];
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                int col = dataGridViewGamer2.CurrentCell.ColumnIndex + 1;
+                int row = dataGridViewGamer2.CurrentCell.RowIndex;
+                dataGridViewGamer2.CurrentCell = dataGridViewGamer2[col, row];
+            }
+        }
+        static word[]ToWord(string[]a)
+        {
+            word[]temp=new word[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                temp[i] = new word(a[i]);
+            }
+            return temp;
+        }
+
+        private void timerChangePicture_Tick(object sender, EventArgs e)
+        {
+            cnt++;
+            pictureBoxShow.Image=list[cnt].GetPicture();
+            labelWord.Text=list[cnt].LoadRusWord();
         }
     }
 }
