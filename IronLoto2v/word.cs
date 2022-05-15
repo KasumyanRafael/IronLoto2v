ef4ab157dcca10d9ae53087a06196ecc81af8b75
KasumyanRafael <Kasumyan2004@mail.ru>
2022-05-15 14:48:12 +0300

    Комментарии в классе

--- IronLoto2v/word.cs
+++ IronLoto2v/word.cs
@@ -18,39 +18,39 @@ namespace IronLoto2v
             string[]s=ss.Split(' ');
             mas = s;
         }        
-        public string LoadRusWord()
+        public string LoadRusWord()  //выгрузка русского слова
         {
             rusword = mas[1];
             return rusword;
         }
-        public string LoadIronWord()
+        public string LoadIronWord() //осетинское слово
         {
             ironword = mas[2];
             return ironword;
         }
-        public Image GetIronWord()
+        public Image GetIronWord() //осетинское слово(в виде изображения)
         {
             string name = "p" + mas[0] + "irword";
             pictureshow = (Image)Properties.Resources.ResourceManager.GetObject(name);
             return pictureshow;
         }
-        public int NumberOf() 
+        public int NumberOf() //номер элемента
         {
             return Convert.ToInt32(mas[0]); 
         }
-        public Image GetPicture()
+        public Image GetPicture() //картинка без надписей
         {
             string name = "p" + mas[0];
             pictureshow=(Image)Properties.Resources.ResourceManager.GetObject(name);
             return pictureshow;
         }
-        public Image GetRusPicture()
+        public Image GetRusPicture() //русская картинка
         {
             string name = "p" + mas[0]+"ru";
             pictureshow = (Image)Properties.Resources.ResourceManager.GetObject(name);
             return pictureshow;
         }
-        public Image GetIrPicture()
+        public Image GetIrPicture() //осетинская картинка
         {
             string name = "p" + mas[0]+"ir";
             pictureshow = (Image)Properties.Resources.ResourceManager.GetObject(name);
