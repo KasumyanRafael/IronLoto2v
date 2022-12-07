44ab9ba9c93e1c033af37e2053192b9895d867e7
KasumyanRafael <Kasumyan2004@mail.ru>
2022-12-07 18:32:57 +0300

    debug

--- IronLoto2v/FormGame.cs
+++ IronLoto2v/FormGame.cs
@@ -35,6 +35,7 @@ namespace IronLoto2v
         }
         public void Start()
         {
+
             regime++;
             this.WindowState = FormWindowState.Maximized;
             firstgamer = new GameUser(labelFirstGamer, labelFirstGamerCount, firstname);
