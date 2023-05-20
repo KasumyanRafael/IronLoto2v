5a8c9ef62809df4e41db324a2b16a304ec920f2e
KasumyanRafael <100214646+KasumyanRafael@users.noreply.github.com>
2023-05-20 15:36:07 +0300

    Добавил неработающие sql запросы

--- IronLoto2v/FormCreateAccount.cs
+++ IronLoto2v/FormCreateAccount.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System.Data.OleDb;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,11 @@ namespace IronLoto2v
 {
     public partial class FormCreateAccount : Form
     {
+        string name;
+        string surname;
+        string password;
+        string email;
+        bool flag = false;
         public FormCreateAccount()
         {
             InitializeComponent();
@@ -21,7 +28,70 @@ namespace IronLoto2v
 
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Аккаунт создан");
+            if(textBoxName.Text!=String.Empty && textBoxName.Text!="Введите имя")
+            {
+                name = textBoxName.Text;
+                if (textBoxSurname.Text != String.Empty && textBoxSurname.Text != "Введите фамилию")
+                {
+                    surname = textBoxName.Text;
+                    if (textBoxEmail.Text != String.Empty && textBoxEmail.Text != "Введите адрес почты")
+                    {
+                        email = textBoxName.Text;
+                        if (textBoxPassword.Text != String.Empty && textBoxPassword.Text != "Введите пароль")
+                        {
+                            if (textBoxCheck.Text != String.Empty && textBoxCheck.Text != "Повторите пароль" && textBoxPassword.Text==textBoxCheck.Text)
+                            {
+                                password = textBoxPassword.Text;
+                                string connect= "Data Source=127.0.0.1;Port=3306;User ID=root;Password=Kolobok168259;database=iron_pictures";
+                                MySqlConnection connection = new MySqlConnection(connect);
+                                string command = "insert into iron_pictures.extendedusers(userFirstName,userLastName,email,passwordInfo) values(@uname,@usurname,@uemail,@upassword)";
+                                MySqlCommand mySqlCommand = new MySqlCommand(command, connection);
+                                mySqlCommand.Parameters.AddWithValue("@uname", name);
+                                mySqlCommand.Parameters.AddWithValue("@usurname", surname);
+                                mySqlCommand.Parameters.AddWithValue("@uemail", email);
+                                mySqlCommand.Parameters.AddWithValue("@upassword", password);
+                                connection.Open();
+                                try
+                                {
+                                    mySqlCommand.ExecuteNonQuery();                                    
+                                }
+                                catch
+                                {
+                                    flag = true;
+                                    MessageBox.Show("Что-то пошло не так");
+                                }
+                                finally
+                                {
+                                    connection.Close();
+                                    if(!flag)MessageBox.Show("Аккаунт создан");
+                                    MessageBox.Show(name);
+                                    this.Hide();
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Повторите пароль");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Поле пароль обязательно");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Поле с электронной почтой обязательно");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Поле фамилия обязательно");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поле имя обязательно");
+            }
         }
     }
 }
